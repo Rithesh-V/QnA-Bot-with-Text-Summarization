da07204f4ecccd30b91472b36be657a82f4b632d
Rithesh-V <132127628+Rithesh-V@users.noreply.github.com>
2023-05-12 18:31:16 +0530

    flask.py

--- flask.py
+++ flask.py
@@ -17,46 +17,57 @@ def home():
 # Results page
 @app.route('/results', methods=['POST'])
 def results():
-    # Get topic from form input
-    topic = request.form['topic']
-    # Scrape Wikipedia page
-    url = f"https://en.wikipedia.org/wiki/{topic}"
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, "html.parser")
-    content_div = soup.find("div", {"id": "bodyContent"})
-    line=''
-    line += " ".join([p.text for p in content_div.find_all("p")])
-    # Preprocess text
-    text = re.sub(r'[^\w\s]', '', line)
-    text = text.lower()
-    words = word_tokenize(text)
-    english_vocab = set(word.lower() for word in nltk.corpus.words.words())
-    stop_words = set(stopwords.words('english'))
-    words = [word for word in words if word not in stop_words]
-    text = ' '.join(words)
-    # Divide text into paragraphs and sentences
-    paragraphs = text.split('\n\n')
-    sentences = []
-    for paragraph in paragraphs:
-        sentences.extend(sent_tokenize(paragraph))
-    # Generate summaries for each part of the document
+    def scrape_wikipedia_pages(topic):
+        url = f"https://en.wikipedia.org/wiki/{topic}"
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, "html.parser")
+        content_div = soup.find("div", {"id": "bodyContent"})
+        line=''
+        line += " ".join([p.text for p in content_div.find_all("p")])
+        return line
+    def preprocess_text(text):
+        text = re.sub(r'[^\w\s]', '', text)
+        text = text.lower()
+        words = word_tokenize(text)
+        english_vocab = set(word.lower() for word in nltk.corpus.words.words())
+        stop_words = set(stopwords.words('english'))
+        words = [word for word in words if word not in stop_words]
+        text = ' '.join(words)
+        return text
+    
+    def divide_documents(text):
+        paragraphs = text.split('\n\n')
+        sentences = []
+        for paragraph in paragraphs:
+            sentences.extend(sent_tokenize(paragraph))
+        return sentences
+    def generate_summaries(sentences):
+        summaries = []
+        for sentence in sentences:
+            summary = sentence
+            summaries.append(summary)
+        return summaries
     model_name = "bert-large-uncased-whole-word-masking-finetuned-squad"
     model = AutoModelForQuestionAnswering.from_pretrained(model_name)
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     qa_pipeline = pipeline("question-answering", model=model, tokenizer=tokenizer)
-    summaries = []
-    for sentence in sentences:
-        summary = sentence
-        summaries.append(summary)
-    # Answer the given question
     question = request.form['question']
-    answers = []
-    for summary in summaries:
-        ans = qa_pipeline(question=question, context=summary)
-        answers.append(ans)
-    answer = answers[0]['answer']
-    # Render results page with answer
-    return render_template('results.html', topic=topic, question=question, answer=answer)
+    def answer_questions(summaries, question):
+        answers = []
+        for summary in summaries:
+            ans = qa_pipeline(question=question, context=summary)
+            answers.append(ans)
+        answer = answers[0]['answer']
+        return answer
+    
+    topic = request.form['topic']
+    text = scrape_wikipedia_pages(topic)
+    text = preprocess_text(text)
+    sentences = divide_documents(text)
+    summaries = generate_summaries(sentences)
+    answer = answer_questions(summaries, question)
+    if answer !=0:
+        return render_template('results.html', topic=topic, question=question, answer=answer)
 
 if __name__ == '__main__':
     app.run(debug=True)
