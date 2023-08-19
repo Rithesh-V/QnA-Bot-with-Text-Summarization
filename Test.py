8383b39b62561b12c4cba8c22c8062f6df0330ac
Rithesh-V <132127628+Rithesh-V@users.noreply.github.com>
2023-08-19 10:44:39 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -7,7 +7,7 @@ import re
 from transformers import pipeline, AutoModelForQuestionAnswering, AutoTokenizer
 
 
-# Scrape Wikipedia pages
+
 def scrape_wikipedia_pages(topic):
     url = f"https://en.wikipedia.org/wiki/{topic}"
     response = requests.get(url)
@@ -17,60 +17,58 @@ def scrape_wikipedia_pages(topic):
     line += " ".join([p.text for p in content_div.find_all("p")])
     return line
 
-# Preprocess text data
 def preprocess_text(text):
-    # Remove unwanted characters
     text = re.sub(r'[^\w\s]', '', text)
-    # Convert to lowercase
+
     text = text.lower()
-    # Tokenize the text into words
+   
     words = word_tokenize(text)
-    # Remove stopwords
+
     english_vocab = set(word.lower() for word in nltk.corpus.words.words())
     stop_words = set(stopwords.words('english'))
     words = [word for word in words if word not in stop_words]
    
-    # Join the words back into sentences
+
     text = ' '.join(words)
     return text
 
-# Divide the documents into smaller parts
+
 def divide_documents(text):
-    # Divide the text into paragraphs
+
     paragraphs = text.split('\n\n')
-    # Divide each paragraph into sentences
+  
     sentences = []
     for paragraph in paragraphs:
         sentences.extend(sent_tokenize(paragraph))
     return sentences
 
-# Generate summaries for each part of the documents using a summarization algorithm
+
 def generate_summaries(sentences):
-    # TODO: Implement summarization algorithm
+  
     summaries = []
     for sentence in sentences:
-        # TODO: Generate summary for each sentence
+
         summary = sentence
         summaries.append(summary)
     return summaries
 
-# Use a Question-Answering algorithm to extract the answers from the summarized data for the given questions
+
 model_name = "bert-large-uncased-whole-word-masking-finetuned-squad"
 model = AutoModelForQuestionAnswering.from_pretrained(model_name)
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-# set up the pipeline with the specified model
+
 qa_pipeline = pipeline("question-answering", model=model, tokenizer=tokenizer)
 def answer_questions(summaries, question):
-    # TODO: Implement Question-Answering algorithm
+   
     answers = []
     for summary in summaries:
-        # TODO: Extract answer from each summary for the given question
+       
         ans = qa_pipeline(question=question, context=summary)
         answers.append(ans)
     return answers[0]['answer']
 
-# Example usage
+
 number = int(input("Enter the number of topics: "))
 for i in range(number):
     topic=''
